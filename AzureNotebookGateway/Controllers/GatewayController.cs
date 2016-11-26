912fbcdd7d35cfde9ff412a74c153403b45c83d3
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-11-26 01:14:56 +0000

    Version 0

---
+++ AzureNotebookGateway/Controllers/GatewayController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AzureNotebookGateway.Controllers
+{
+    public class GatewayController : ApiController
+    {
+        // GET api/values
+        public async Task<HttpResponseMessage> Get([FromUri(Name ="q")]string url)
+        {
+            var cli = new HttpClient();
+            var ba = await cli.GetByteArrayAsync(url);
+            var hr = new HttpResponseMessage(HttpStatusCode.OK);
+            // hr.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"image/jpeg");
+            hr.Content = new ByteArrayContent(ba);
+            /*            hr.Content.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("image/jpeg");
+                        hr.Content.Headers.ContentLength = ba.Length; */
+            hr.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+            hr.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            hr.Content.Headers.ContentDisposition.FileName = new Uri(url).LocalPath;
+            return hr;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AzureNotebookGateway
-Gateway to allow passing arbitrary files to Azure Notebooks
+
+### Gateway to allow passing arbitrary files to Azure Notebooks
+
+Unfortunately, [Azure Notebooks](http://notebooks.azure.com) limit access to external data from a few sources. This gateway
+allows your Azure notebooks to download data (in particular, images) from any place on the internet.
+
+To use it:
+
+    * Deploy the project somewhere on Azure (eg. http://az.azurewebsites.net)
+    * In your notebook, substitute URLs like http://image.net/image.jpg to http://az.azurewebsites.net/api/gateway?q=https%3A%2F%2Fimages.net%2Fimage.jpg
+    * Enjoy!
+
